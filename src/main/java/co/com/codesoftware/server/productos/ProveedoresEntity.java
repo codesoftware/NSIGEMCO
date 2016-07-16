132debab3d6c089069f54c12e70cb73a81a9bbfd
jnsierra <jnsierrac@gmail.com>
2016-07-16 16:44:13 -0500

    16/07/2016 003
    Ajustes basicos

--- src/main/java/co/com/codesoftware/server/productos/ProveedoresEntity.java
+++ src/main/java/co/com/codesoftware/server/productos/ProveedoresEntity.java
@@ -1,6 +1,7 @@
 
 package co.com.codesoftware.server.productos;
 
+import java.math.BigDecimal;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
@@ -19,13 +20,16 @@ import javax.xml.bind.annotation.XmlType;
  *         &lt;element name="celular" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="ciudad" type="{http://productos.server.codesoftware.com.co/}ciudadEntity" minOccurs="0"/>
  *         &lt;element name="correo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="dcredito" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
  *         &lt;element name="direccion" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="dverificacion" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="estado" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="granContribuyente" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
  *         &lt;element name="municipio" type="{http://productos.server.codesoftware.com.co/}departamentoEntity" minOccurs="0"/>
  *         &lt;element name="nit" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="porcRetencion" type="{http://www.w3.org/2001/XMLSchema}decimal" minOccurs="0"/>
  *         &lt;element name="razonSocial" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="representante" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="retenciones" type="{http://productos.server.codesoftware.com.co/}reteFuenteEntity" minOccurs="0"/>
@@ -43,13 +47,16 @@ import javax.xml.bind.annotation.XmlType;
     "celular",
     "ciudad",
     "correo",
+    "dcredito",
     "direccion",
+    "dverificacion",
     "estado",
     "granContribuyente",
     "id",
     "municipio",
     "nit",
     "nombre",
+    "porcRetencion",
     "razonSocial",
     "representante",
     "retenciones",
@@ -60,13 +67,16 @@ public class ProveedoresEntity {
     protected String celular;
     protected CiudadEntity ciudad;
     protected String correo;
+    protected Integer dcredito;
     protected String direccion;
+    protected String dverificacion;
     protected String estado;
     protected String granContribuyente;
     protected Integer id;
     protected DepartamentoEntity municipio;
     protected String nit;
     protected String nombre;
+    protected BigDecimal porcRetencion;
     protected String razonSocial;
     protected String representante;
     protected ReteFuenteEntity retenciones;
@@ -145,6 +155,30 @@ public class ProveedoresEntity {
     }
 
     /**
+     * Obtiene el valor de la propiedad dcredito.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getDcredito() {
+        return dcredito;
+    }
+
+    /**
+     * Define el valor de la propiedad dcredito.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setDcredito(Integer value) {
+        this.dcredito = value;
+    }
+
+    /**
      * Obtiene el valor de la propiedad direccion.
      * 
      * @return
@@ -166,6 +200,30 @@ public class ProveedoresEntity {
      */
     public void setDireccion(String value) {
         this.direccion = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad dverificacion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDverificacion() {
+        return dverificacion;
+    }
+
+    /**
+     * Define el valor de la propiedad dverificacion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDverificacion(String value) {
+        this.dverificacion = value;
     }
 
     /**
@@ -310,6 +368,30 @@ public class ProveedoresEntity {
      */
     public void setNombre(String value) {
         this.nombre = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad porcRetencion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getPorcRetencion() {
+        return porcRetencion;
+    }
+
+    /**
+     * Define el valor de la propiedad porcRetencion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setPorcRetencion(BigDecimal value) {
+        this.porcRetencion = value;
     }
 
     /**
