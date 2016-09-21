5df2d06899b0428e38279d7faf57c09e4964fb01
jnsierra <jnsierrac@gmail.com>
2016-09-20 19:16:05 -0500

    20/09/2016 001
    Ajustes para las notas credito

--- src/main/java/co/com/codesoftware/beans/facturacion/NotaCreditoBean.java
+++ src/main/java/co/com/codesoftware/beans/facturacion/NotaCreditoBean.java
@@ -93,6 +93,21 @@ public class NotaCreditoBean implements GeneralBean {
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * Funcion con la cual cambio los valores de la cuenta
+	 */
+	public void cambioValoresFactura(){
+		try{
+			System.out.println("Paso por aqui:  " + this.codigoExternoAux + " Precio Auxiliar: " + this.precioAux+ " Cantidad: " + this.cantidad  );
+			for(DetProduFacturaEntity item : this.factura.getDetalleProductos()){
+				if(this.codigoExternoAux.equalsIgnoreCase(item.getProducto().getCodigoExt())){
+					item.setCantidad(this.cantidad);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public UsuarioEntity getObjetoSesion() {
 		return objetoSesion;
